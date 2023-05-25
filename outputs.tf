91a5cbf50fdd47458d353ab54a9a4ecd71efcd6f
Dutt Pathak <dutt.pathak@kloudpath.io>
2023-05-25 12:39:49 -0500

    outputs.tf subnets.tf variables.tf

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+
+# output "public_ip" {
+#   value       = aws_instance.example.public_ip
+#   description = "The public IP address of the web server"
+# }
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
+
+# replace the old public_ip output of the single EC2 Instance you had 
+# before with an output that shows the DNS name of the ALB:
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
