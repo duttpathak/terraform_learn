7ff474ee35ea0fc3b41f337c066a16cd48bb4d2b
Dutt Pathak <dutt.pathak@kloudpath.io>
2023-05-29 20:41:52 -0500

    user_data changed input

--- main.tf
+++ main.tf
@@ -6,14 +6,6 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "example" {
-  ami           =  "ami-0889a44b331db0194" 
-  instance_type =  "t2.micro"
-  tags = {
-    Name = "terraform-example"
-  }
-}
-
 # Provider - Name of a provider (eg aws)
 # Type - Type of resource to create in provider (eg instant)
 # Name - an identifier to refere to this resource
@@ -35,23 +27,77 @@ resource "aws_instance" "example" {
 # You pass a shell script to User Data by setting the 
 # user_data argument in your Terraform code as follows:
 
+# aws instance means virtual machine
+# virtual machine is a software (OS) and application 
+# which runs on existing hardware
 
-# resource "aws_instance" "example" {
-#   ami                    = "ami-0889a44b331db0194"
-#   instance_type          = "t2.micro"
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               echo "Hello, World" > index.html
-#               nohup busybox httpd -f -p 8080 &
-#               EOF
+resource "aws_instance" "example" {
+  # Running Amazon Linux 2023 AMI 
+  # 2023.0.20230503.0 x86_64 HVM kernel-6.1
+  ami                    = "ami-0889a44b331db0194"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.terraform.key_name
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  # uses user_data to install applications (web server)  
+  # on the instance 
+  # http web server
+  # curl command reaches the http web server 
+  # started by the user_data
+  # 
+  user_data              = <<-EOF
+        #!/bin/bash
+        # echo "Hello, World" > index.html
+        # nohup busybox httpd -f -p 8080 &
+        # fixes latest bugs and updates 
+        # do to: match this instance on ec2
+        sudo dnf update -y
+        # install the http server 
+        sudo dnf install -y httpd
+        # starts the http server
+        sudo systemctl start httpd
+        EOF
 
-#   user_data_replace_on_change = true
+  user_data_replace_on_change = true
 
-#   tags = {
-#     Name = "terraform-example"
-#   }
-# }
+  tags = {
+    Name = "terraform-example"
+  }
+}
 
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    # This IP address range 0.0.0.0/0 includes all. 
+    # This allows incoming requests on port 8080 from any IP.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    # This IP address range 0.0.0.0/0 includes all. 
+    # This allows incoming requests on port 8080 from any IP.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    # to do: what port for full outbound excess 
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    # This IP address range 0.0.0.0/0 includes all. 
+    # This allows incoming requests on port 8080 from any IP.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+}
+# This code creates a new resource called aws_security_group
+# and specifies that this group allows incoming TCP requests
+# on port 8080 from the CIDR block.
 
 # Step 3: 
 
@@ -65,174 +111,12 @@ resource "aws_instance" "example" {
 # <provider>_<Type>.<Name>.<Attribute>
 
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  ingress {
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
-    # This IP address range 0.0.0.0/0 includes all. 
-    # This allows incoming requests on port 8080 from any IP.
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-# This code creates a new resource called aws_security_group
-# and specifies that this group allows incoming TCP requests
-# on port 8080 from the CIDR block.
 
 
 
-# The first step is to create the 
-# ALB itself using the aws_lb resource:
-
-resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
+resource "aws_key_pair" "terraform" {
+  key_name   = "terraform"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBGrHCC95QsUPZozICYflAVpTHe0gcBUDklM3CYmYqUvwHXNSRoZEQWv+HMMNHAekDg3UKG27l+mYCqHiOOHMCmNjPBa6JvwBHTlBI0MHnZao2IZbRph72+3BQ3tIErXQeHhcgjbqQYMsrJRNqof4kUzNztQc6eKWyrMPWtgN7KmhU4efA03tsEwM2cl89YKHxAh5BkKIgbwPXwWyRCSl8N/cBgDNXA3rtHw/gAzunBW2GpB4CQekng9ddShOzn5vgs4ODoDOvyHSl1boj2cMu3/R95c8VgsralyobQ6s4NN7WhjtJmfumbz5kAfD4zAu3UFm5JJBRhjv7hLXIkrq7LKGpgoGkPEGKV01pK61sSYEngesZ3knjQXWanmJqbLdHhH+pzZG2zmGeI5BzOsHuSPdA9+pPXO/glZg5zK8aiSAWjmNGcPS1h1Tfbz4zk8fSGYL7HB0vTVOt4GPkwv9MEUr/hZUsJ+uk1G4t6lrglgrqCY3UvT0JXh0etlPWrtREtYXNYLOoYM44IcJKWGeLSjyIpe0IVyNldvdfWiJe5fnabmCIJhOaep2xClMuCqhkpBIVpzLFcEEqupGoJSH0G5i8xlQ/V9C5mL4xuF3IN1ah+gHNqb4PXGxXpSCxfE3jmgBG0GcBymQPONnjPjOiMwRvx6qCbowhQ+LdyxVfmQ== tarpanpathak720@gmail.com"
 }
 
-# The next step is to define a listener for 
-# this ALB using the aws_lb_listener resource:
-
-# This listener configures the ALB to listen on the default 
-# HTTP port, port 80, use HTTP as the protocol, and send 
-# a simple 404 page as the default response 
-# for requests that don’t match any listener rules.
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Next, you need to create a target group for your 
-# ASG using the aws_lb_target_group resource:
-# This target group will health check your Instances 
-# by periodically sending an HTTP request to each Instance 
-# and will consider the Instance “healthy” only if the Instance 
-# returns a response that matches the configured matcher
-
-resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-
-
-
-resource "aws_launch_configuration" "example" {
-  image_id      = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
-  # List of security group IDs to associate with. 
-  security_groups = [aws_security_group.instance.id]
-  # User data to provide when launching the instance. 
-  # Updates to this field will trigger a start/stop of 
-  # EC2 instance by default. 
-  # Line 103 used to create an index.html file and with 
-  # Hello, World in it. 
-  # Line 104  used to run the script even after you log off. 
-  # It continues to run until it is finished. 
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  # Required when using a launch configuration with an ASG.
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-# creating Auto Scaling Group itself
-# instructs the ASG to use the target group’s health check 
-# to determine whether an Instance is healthy and to automatically replace 
-# Instances if the target group reports them as unhealthy.
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 1
-  max_size = 2
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-
-
-
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-
-
-
+# https instead of busybox
