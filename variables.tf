6b493f1cc02d6d34faf9b191ab04312a53d9ce62
Dutt Pathak <dutt.pathak@kloudpath.io>
2023-05-31 18:04:30 -0500

    defined variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,10 @@ variable "server_port" {
   description = "The port the server will use for HTTP requests"
   type        = number
 }
+
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
+}
