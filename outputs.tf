d6090ecb88c05b08b27e323afd9e1c3c08bfd62d
Tarpan Pathak <tarpanpathak@tarpans-mbp.lan>
2023-06-13 20:42:01 -0700

    Fixed and tested the user data logic w/o the load balancer

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,8 @@
 
-output "public_ip" {
-  value       = "curl http://${aws_instance.example.public_ip}"
-  description = "The public IP address of the web server"
-}
-
-
+# output "public_ip" {
+#   value       = "curl http://${aws_instance.example.public_ip}"
+#   description = "The public IP address of the web server"
+# }
 
 # # replace the old public_ip output of the single EC2 Instance you had 
 # # before with an output that shows the DNS name of the ALB:
