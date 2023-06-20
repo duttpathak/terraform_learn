aed39feb236e72ad6b988eaacb92d3aac884c45b
Dutt Pathak <dutt.pathak@kloudpath.io>
2023-06-20 09:52:14 +0530

    succefully ran multiple servers

--- asg.tf
+++ asg.tf
@@ -36,8 +36,8 @@ EOF
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnets.default.ids
-  #   target_group_arns    = [aws_lb_target_group.asg.arn]
-  #   health_check_type = "ELB"
+    target_group_arns    = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
   min_size = 1
   max_size = 1
   tag {
--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@
 # # replace the old public_ip output of the single EC2 Instance you had 
 # # before with an output that shows the DNS name of the ALB:
 
-# output "alb_dns_name" {
-#   value       = aws_lb.example.dns_name
-#   description = "The domain name of the load balancer"
-# }
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
