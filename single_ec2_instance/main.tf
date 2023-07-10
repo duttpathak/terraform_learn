18d1402c5aeec1601565c4ae0f913401a8b0b195
Dutt Pathak <dutt.pathak@kloudpath.io>
2023-07-10 13:41:12 -0500

    created new folde single_ec2_instance

--- single_ec2_instance/main.tf
+++ single_ec2_instance/main.tf
@@ -139,3 +139,35 @@ output "public_ip" {
 }
 
 # github.com/duttpathak/terraform_learn/main
+
+
+
+
+
+
+# variable "server_port" {
+#   description = "The port the server will use for HTTP requests"
+#   type        = number
+# }
+
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 80
+}
+
+
+variable "server_port_in" {
+  description = "The port for incoming HTTP requests"
+  type        = number
+  default     = 22
+}
+
+
+variable "server_port_out" {
+  description = "The port for outgoing HTTP requests"
+  type        = number
+  default     = 65535
+
+}
